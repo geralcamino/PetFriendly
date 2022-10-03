b7c017fde81f9b9945b02cdc8ceec15a8604b81b
geralcamino <65680820+geralcamino@users.noreply.github.com>
2022-10-02 22:00:29 -0500

    firebase

---
+++ firebase.js
@@ -0,0 +1,20 @@
+console.log('todo ok');
+
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDnpnXJsxMxn33RV2rPjGMDD1gnwDQD4z0",
+    authDomain: "petfriendly-5a8d3.firebaseapp.com",
+    projectId: "petfriendly-5a8d3",
+    storageBucket: "petfriendly-5a8d3.appspot.com",
+    messagingSenderId: "990910792060",
+    appId: "1:990910792060:web:c71269018d234c4fd5a4f9"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
--- main.js
+++ main.js
@@ -1,21 +1,5 @@
-function show() {
-    var p = document.getElementById('pwd');
-    p.setAttribute('type', 'text');
-}
 
-function hide() {
-    var p = document.getElementById('pwd');
-    p.setAttribute('type', 'password');
-}
+window.addEventListener('DOMContentLoaded',()=>{
+console.log('holi')
 
-var pwShown = 0;
-
-document.getElementById("eye").addEventListener("click", function () {
-    if (pwShown == 0) {
-        pwShown = 1;
-        show();
-    } else {
-        pwShown = 0;
-        hide();
-    }
-}, false);
+} )
