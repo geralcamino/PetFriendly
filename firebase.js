6de42430b55014cdf0a71df1bd401b4670f8ec66
geralcamino <65680820+geralcamino@users.noreply.github.com>
2022-10-02 22:42:41 -0500

    conexion_bd

--- firebase.js
+++ firebase.js
@@ -3,6 +3,7 @@ console.log('todo ok');
 
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+  import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,3 +19,8 @@ console.log('todo ok');
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  const db = getFirestore()
+
+  export const Formulario = (Usuario,contrasenia) => {
+   addDoc( collection(db,"formulario"), {Usuario, contrasenia});
+}
