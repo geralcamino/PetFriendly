233473b4d3a814ea8f1eb24f8a2706a2e7025c4e
geralcamino <65680820+geralcamino@users.noreply.github.com>
2022-10-19 19:27:53 -0500

    login
    conexion_firabase

--- firebase.js
+++ firebase.js
@@ -1,26 +1,30 @@
 console.log('todo ok');
-
-
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
-  import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
+  import { initializeApp } from 'firebase/app';
+  import {getAuth, onAuthStateChanged} from 'firebase/auth';
+  import {getFirestore, collection,getDocs, getDoc } from 'firebase/firestore';
+
+  
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
-  const firebaseConfig = {
+  const firebaseConfig = initializeApp({
     apiKey: "AIzaSyDnpnXJsxMxn33RV2rPjGMDD1gnwDQD4z0",
     authDomain: "petfriendly-5a8d3.firebaseapp.com",
     projectId: "petfriendly-5a8d3",
     storageBucket: "petfriendly-5a8d3.appspot.com",
     messagingSenderId: "990910792060",
     appId: "1:990910792060:web:c71269018d234c4fd5a4f9"
-  };
+  });
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore()
 
-  export const Formulario = (Usuario,contrasenia) => {
-   addDoc( collection(db,"formulario"), {Usuario, contrasenia});
-}
+const auth = getAuth(firebaseConfig);
+const db = getFirestore(firebaseConfig);
+
+
+
+
+
+
+
